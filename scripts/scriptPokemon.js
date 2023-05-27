8ff2eccf85a4c100bca4cbcfd242d593026186af
Kylian Thezenas <kylian.thezenas@etu.umontpellier.fr>
2023-05-27 15:14:21 +0200

    bouton supprimer et ajouter pokemon

--- scripts/scriptPokemon.js
+++ scripts/scriptPokemon.js
@@ -5,7 +5,7 @@ USER_NAME = localStorage.getItem('name');
 
 function isFavorite(pokemonName) {
   return new Promise((resolve, reject) => {
-    fetch('https://pokydexapi.cluster-ig3.igpolytech.fr/favoris/api/favoris/')
+    fetch('http://localhost:5000' + '/favoris/api/favoris/')
       .then(response => response.json())
       .then(data => {
         for (const favoris of data) {
@@ -23,8 +23,34 @@ function isFavorite(pokemonName) {
   });
 }
 
+// function qui verifie que l'utilisateur est admin
+function isAdmin() {
+  return new Promise((resolve, reject) => {
+    fetch('http://localhost:5000' + '/user/api/user/connected', {
+      method: 'GET',
+      headers: {
+        Authorization: 'Bearer ' + USER_TOKEN
+      }
+    })
+      .then(response => response.json())
+      .then(data => {
+        if (data[0].role === "admin") {
+          resolve(true);
+        } else {
+          resolve(false);
+        }
+      })
+      .catch(error => {
+        console.error('Une erreur s\'est produite lors de la récupération des données utilisateur:', error);
+        reject(error);
+      });
+  });
+}
+
 
 function createPokemonCard(pokemon) {
+  const card_container = document.createElement('div');
+  card_container.classList.add('card-container');
   const card = document.createElement('div');
   card.classList.add('card');
   card.classList.add('large-card');
@@ -66,7 +92,7 @@ function createPokemonCard(pokemon) {
 
   favoriteButton.addEventListener('click', function () { 
     if (favoriteButton.textContent === 'Ajouter aux favoris') {
-      fetch('https://pokydexapi.cluster-ig3.igpolytech.fr/favoris/api/favoris', {
+      fetch('http://localhost:5000' + '/favoris/api/favoris', {
           method: 'POST',        
           headers: {
           'Content-Type': 'application/json'
@@ -77,7 +103,7 @@ function createPokemonCard(pokemon) {
       }
       else {
         
-        fetch('https://pokydexapi.cluster-ig3.igpolytech.fr/favoris/api/favoris/'+USER_NAME+'/'+pokemon.name, {
+        fetch('http://localhost:5000' + '/favoris/api/favoris/'+USER_NAME+'/'+pokemon.name, {
           method: 'DELETE',        
           headers: {
           'Content-Type': 'application/json'
@@ -103,19 +129,13 @@ function createPokemonCard(pokemon) {
   card.appendChild(front);
   card.appendChild(back);
 
-  card.addEventListener('mouseenter', function() {
-    front.classList.add('hidden');
-  });
-
-  card.addEventListener('mouseleave', function() {
-    front.classList.remove('hidden');
-  });
+  card_container.appendChild(card);
 
   card.addEventListener('click', function() {
     if (event.target !== favoriteButton){openModal(pokemon);}
   });
 
-  return card;
+  return card_container;
 }
 
 function openModal(pokemon) {
@@ -155,7 +175,7 @@ function openModal(pokemon) {
   numberElement.textContent = 'n° : ' + pokemon.number;
 
   const typeElement = document.createElement('img');
-  typeElement.src = '../images/'+pokemon.type+'.png';
+  typeElement.src = 'images/'+pokemon.type+'.png';
 
   const descriptionElement = document.createElement('p');
   descriptionElement.textContent = pokemon.description;
@@ -210,7 +230,7 @@ function populateGrid(pokemonData) {
 // Fonction pour filtrer les Pokémon en fonction de la saisie de l'utilisateur
 function filterPokemons(searchString, selectedTypes) {
   const grid = document.getElementById('grid');
-  const pokemons = grid.getElementsByClassName('card');
+  const pokemons = grid.getElementsByClassName('card-container');
 
   // Parcourir tous les Pokémon de la grille
   for (const pokemon of pokemons) {
@@ -291,7 +311,7 @@ typeCheckboxes.forEach(function (checkbox) {
 
 
 // Récupération des données de Pokémon à partir du backend
-fetch('https://pokydexapi.cluster-ig3.igpolytech.fr/pokemon/api/pokemon')
+fetch('http://localhost:5000' + '/pokemon/api/pokemon')
   .then(response => response.json())
   .then(data => {
     // Traiter les données JSON 
@@ -312,7 +332,7 @@ favCheckbox.addEventListener('change', function () {
     while (grid.firstChild) {
       grid.removeChild(grid.firstChild);
     }
-    fetch('https://pokydexapi.cluster-ig3.igpolytech.fr/pokemon/api/pokemon')
+    fetch('http://localhost:5000' + '/pokemon/api/pokemon')
       .then(response => response.json())
       .then(data => {
         // Traiter les données JSON 
@@ -331,3 +351,181 @@ favCheckbox.addEventListener('change', function () {
 
 
 
+// bouton d'ajout de pokemon pour les admins
+const addPokemonButton = document.getElementById('addPokemonButton');
+const deletePokemonButton = document.getElementById('deletePokemonButton');
+
+if (USER_TOKEN) {
+  //retire la classe hidden si l'utilisateur est admin
+  isAdmin().then(isAdmin => {
+    if (isAdmin) {
+      addPokemonButton.classList.remove('hidden');
+      deletePokemonButton.classList.remove('hidden');
+    }
+  });
+}
+
+var contenu = '';
+// function pour bouton d'ajout de pokemon
+function ouvrirFenetreSuppression() {
+    // Création de la div de superposition
+
+  var overlay = document.createElement('div');
+  overlay.setAttribute('id', 'overlay');
+  overlay.setAttribute('style', 'position: fixed; top: 0; left: 0; width: 100%; height: 100%; background-color: rgba(0, 0, 0, 0.5); z-index: 9999;');
+
+    // Contenu de la fenêtre
+  var contenu = '<div class="fenetre" style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); background-color: white; padding: 20px; width: 400px;">';
+  contenu += '<span class="fermer" onclick="fermerFenetre()">X</span>';
+  contenu += '<h2>Quel Pokemon supprimer du pokedex ?</h2>';
+
+  // Champs à renseigner
+  contenu += 'Numéro : <input type="text" name="numero"><br>';
+  contenu += '<button type="button" onclick="rechercherPokemon()">Rechercher</button>';
+  contenu += '<button type="button" onclick="soumettreFormulaireSuppression()">Supprimer</button>';
+  contenu += '</div>';
+
+  overlay.innerHTML = contenu;
+
+  // Ajout de la superposition à la page
+  document.body.appendChild(overlay);
+}
+
+function fermerFenetre() {
+  document.body.removeChild(overlay); // Supprime la superposition de la page
+}
+
+function rechercherPokemon() {
+  var input = document.querySelector('input[name="numero"]');
+  var inputValue = input.value;
+
+  fetch('http://localhost:5000' + '/pokemon/api/pokemon/' + inputValue)
+  .then(response => response.json())
+  .then(data => {
+    var pokemonFound = data;
+    if (pokemonFound) {
+      openModal(pokemonFound[0]);
+    } else {
+      var message = '<p>Aucun pokemon avec ce numéro dans le pokedex</p>';
+      contenu = contenu + message; // Met à jour la variable contenu
+      overlay.innerHTML = contenu; // Met à jour le contenu de la superposition
+    }
+  });
+}
+  
+function soumettreFormulaireSuppression(event) {
+  var input = document.querySelector('input[name="numero"]');
+  var inputValue = input.value;
+
+  fetch('http://localhost:5000' + '/pokemon/api/pokemon/' + inputValue)
+  .then(response => response.json())
+  .then(data => {
+    var pokemonFound = data;
+    if (pokemonFound) {
+      openModal(pokemonFound[0]);
+      fetch('http://localhost:5000' + '/pokemon/api/pokemon/' + inputValue, {
+        method: 'DELETE',
+        headers: {
+          Authorization: 'Bearer ' + USER_TOKEN,
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+        })
+        .catch(error => {
+          console.error('Une erreur s\'est produite lors de la suppression du pokemon:', error);
+        });
+    } else {
+      var message = '<p>Aucun pokemon avec ce numéro dans le pokedex</p>';
+      contenu = contenu + message; // Met à jour la variable contenu
+      overlay.innerHTML = contenu; // Met à jour le contenu de la superposition
+    }
+  });
+  
+  var input = document.querySelector('input[name="numero"]');
+    // Suppression de la superposition de la page
+  var overlay = document.getElementById('overlay');
+  var input = document.querySelector('input[name="numero"]');
+  overlay.parentNode.removeChild(overlay); 
+
+
+}
+  
+
+
+
+// function pour bouton d'ajout de pokemon
+function ouvrirFenetreAjout() {
+  // Création de la div de superposition
+var overlay = document.createElement('div');
+overlay.setAttribute('id', 'overlay');
+overlay.setAttribute('style', 'position: fixed; top: 0; left: 0; width: 100%; height: 100%; background-color: rgba(0, 0, 0, 0.5); z-index: 9999;');
+
+  // Contenu de la fenêtre
+var contenu = '<div style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); background-color: white; padding: 20px; width: 400px;">';
+contenu += '<h2>Renseignez les champs</h2>';
+contenu += '<form onsubmit="return soumettreFormulaireAjout(event);">';
+
+// Champs à renseigner
+contenu += 'Nom : <input type="text" name="name"><br>';
+contenu += 'Numéro : <input type="number" name="numero"><br>';
+contenu += 'Description : <input type="text" name="description"><br>';
+
+  contenu += 'Type : <br>';
+  var types = ['Normal', 'Feu', 'Eau', 'Electrik', 'Plante', 'Glace', 'Combat', 'Poison', 'Sol', 'Vol', 'Psy', 'Insecte', 'Roche', 'Spectre', 'Dragon', 'Tenebre', 'Acier', 'Fee'];
+  for (var i = 0; i < types.length; i++) {
+    contenu += '<input type="radio" name="type" value="' + types[i] + '"> ' + types[i] + '<br>';
+  }
+
+ // Bouton de soumission
+contenu += '<input type="submit" value="Soumettre">';
+contenu += '</form>';
+contenu += '</div>';
+
+overlay.innerHTML = contenu;
+
+// Ajout de la superposition à la page
+document.body.appendChild(overlay);
+}
+
+function soumettreFormulaireAjout(event) {
+  event.preventDefault();
+
+  var name = document.querySelector('input[name="name"]').value;
+  var numero = parseInt(document.querySelector('input[name="numero"]').value);
+  var description = document.querySelector('input[name="description"]').value;
+
+  var typeElement = document.querySelector('input[name="type"]:checked');
+  var type = typeElement ? typeElement.value : null;
+
+  var body = {
+    "name": name,
+    "number": numero,
+    "description": description,
+    "type": type
+  };
+
+  console.log(body);
+
+  fetch('http://localhost:5000' + '/pokemon/api/pokemon', {
+    method: 'POST',
+    headers: {
+      Authorization: 'Bearer ' + USER_TOKEN,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(body)
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+    })
+    .catch(error => {
+      console.error('Une erreur s\'est produite lors de la création du pokemon:', error);
+    });
+
+  // Suppression de la superposition de la page
+var overlay = document.getElementById('overlay');
+overlay.parentNode.removeChild(overlay); 
+}
